4628462a2aec77559721198be2ecc724e4f61e9b
yuki-amir <amir@yukidata.com>
2024-11-12 13:14:15 +0200

    fix macro

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,14 +1,10 @@
 {% macro set_query_tag() %}
-    {% if target.type == 'snowflake' %}
-        {% set model_name = this.name %}
-        {% set job_name = var('job_name', 'default_job') %}
+    {% set model_name = this.name %}
+    {% set job_name = env_var('DBT_JOB_NAME', 'default_job') %}  -- Retrieve from environment variable, or use 'default_job'
 
-        -- JSON formatted query tag
-        {% set query_tag = '{"dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '"}' %}
+    -- Create JSON formatted query tag
+    {% set query_tag = '{"dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '"}' %}
 
-        -- Set the query tag in Snowflake
-        ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
-    {% else %}
-        {{ log("set_query_tag is only supported on Snowflake connections.", info=True) }}
-    {% endif %}
+    -- Set the query tag in Snowflake
+    ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
 {% endmacro %}
