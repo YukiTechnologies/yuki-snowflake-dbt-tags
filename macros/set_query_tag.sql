61caa319850de3f9e3026963d4bdc49c577bc5db
isayyuki <156585792+isayyuki@users.noreply.github.com>
2024-11-13 16:21:20 +0200

    Update set_query_tag.sql
    Delete commits

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -3,11 +3,7 @@
         {% set model_name = this.name %}
         {% set job_name = var('job_name', 'default_job') %}
         {% set job_id = generated_job_id %}
-
-        -- JSON formatted query tag
         {% set query_tag = '{"dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '", "job_id":"' ~ job_id ~ '"}' %}
-
-        -- Set the query tag in Snowflake
         ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
     {% else %}
         {{ log("set_query_tag is only supported on Snowflake connections.", info=True) }}
