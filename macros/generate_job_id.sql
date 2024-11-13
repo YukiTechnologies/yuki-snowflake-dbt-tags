8fb637b55c958cbac52b28639832e1482b05e7f5
yuki-amir <amir@yukidata.com>
2024-11-13 15:48:57 +0200

    generate job id with dbt

--- macros/generate_job_id.sql
+++ macros/generate_job_id.sql
@@ -1,18 +1,5 @@
 {% macro generate_job_id() %}
-    {% if target.type == 'snowflake' %}
-        {% set query_result = run_query("SELECT UUID_STRING() as unique_id") %}
-
-        {% if query_result is not none and query_result.columns[0] is not none %}
-            {% set job_id = query_result.columns[0].values()[0] %}
-            {{ log("Generated Job ID: " ~ job_id, info=True) }}
-        {% else %}
-            {{ log("Warning: run_query returned None or unexpected format. Using fallback ID.", info=True) }}
-            {% set job_id = "fallback_id" %}
-        {% endif %}
-    {% else %}
-        {{ log("Non-Snowflake target detected. Using unknown ID.", info=True) }}
-        {% set job_id = "unknown_id" %}
-    {% endif %}
-
+    {% set job_id = uuid() %}
+    {{ log("Generated Job ID: " ~ job_id, info=True) }}
     {{ return(job_id) }}
 {% endmacro %}
