b6e9e82515877f069b2b4022de3ebc5aed2fdf8f
yuki-amir <amir@yukidata.com>
2024-11-13 15:44:46 +0200

    fix job id

--- macros/generate_job_id.sql
+++ macros/generate_job_id.sql
@@ -1,6 +1,11 @@
 {% macro generate_job_id() %}
     {% if target.type == 'snowflake' %}
-        {% set job_id = run_query("SELECT UUID_STRING() as unique_id").columns[0].values()[0] %}
+        {% set query_result = run_query("SELECT UUID_STRING() as unique_id") %}
+        {% if query_result is not none and query_result.columns[0] is not none %}
+            {% set job_id = query_result.columns[0].values()[0] %}
+        {% else %}
+            {% set job_id = "fallback_id" %}
+        {% endif %}
     {% else %}
         {% set job_id = "unknown_id" %}
     {% endif %}
