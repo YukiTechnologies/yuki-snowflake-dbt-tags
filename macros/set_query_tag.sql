5f887e7cbfed25c0fbbc3833b0e34ab9450639e3
isayyuki <156585792+isayyuki@users.noreply.github.com>
2024-11-14 12:34:27 +0200

    Update set_query_tag.sql
    Update to job

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,11 +1,7 @@
 {% macro set_query_tag() %}
-    {% if target.type == 'snowflake' %}
-        {% set model_name = this.name %}
-        {% set job_name = var('job_name', 'default_job') %}
-        {% set job_id = generated_job_id %}
-        {% set query_tag = '{"dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '", "job_id":"' ~ job_id ~ '"}' %}
-        ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
-    {% else %}
-        {{ log("set_query_tag is only supported on Snowflake connections.", info=True) }}
-    {% endif %}
+    {% set model_name = this.name %}
+    {% set job_name = var('job_name', 'default_job') %}
+    {% set job_started_at = run_started_at.strftime('%Y-%m-%dT%H:%M:%S') %}
+    {% set query_tag = '{"dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '", "job_started_at":"' ~ job_started_at ~ '"}' %}
+    ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
 {% endmacro %}
