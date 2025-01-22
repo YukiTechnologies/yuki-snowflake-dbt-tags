b327ce7e981f4548c687aba8aa9690bc16731108
Isay Razilov <isay@yukidata.com>
2025-01-22 11:03:47 +0200

    Add full refresh to macro

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,9 +1,11 @@
 {% macro set_query_tag() %}
-    {% set model_name = this.name %}
-    {% set job_name = env_var('DBT_JOB_NAME') %}
-    {% set yuki_enabled = env_var('DBT_YUKI_ENABLED') %}
-    {% set job_started_at = run_started_at.strftime('%Y-%m-%dT%H:%M:%S') %}
-    {% set query_tag = '{"dbt_enabled":"' ~ yuki_enabled ~ '", "dbt_job":"' ~ job_name ~ '", "dbt_model":"' ~ model_name ~ '", "job_started_at":"' ~ job_started_at ~ '"}' %}
+    {% set query_tag = {
+        "dbt_enabled": env_var('DBT_YUKI_ENABLED'),
+        "dbt_job": env_var('DBT_JOB_NAME'),
+        "dbt_model": this.name,
+        "job_started_at": run_started_at.strftime('%Y-%m-%dT%H:%M:%S'),
+        "full_refresh": flags.FULL_REFRESH
+    } | tojson %}
 
 ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
 {% endmacro %}
