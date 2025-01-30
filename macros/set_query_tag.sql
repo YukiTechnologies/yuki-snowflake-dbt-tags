7675743c96d11b1d0f81c73df5947ae864ece0a5
Isay Razilov <isay@yukidata.com>
2025-01-30 13:03:41 +0200

    db_enabled Update default value

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,6 +1,6 @@
 {% macro set_query_tag() %}
     {% set query_tag = {
-        "dbt_enabled": env_var('DBT_YUKI_ENABLED', 'true'),
+        "dbt_enabled": env_var('DBT_YUKI_ENABLED', 'True'),
         "dbt_job": env_var('DBT_JOB_NAME'),
         "dbt_model": this.name,
         "job_started_at": run_started_at.strftime('%Y-%m-%dT%H:%M:%S'),
