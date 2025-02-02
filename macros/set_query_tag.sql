45b8305b2407fb2313b5faa6af31a52b4b4f9d3c
Isay Razilov <isay@yukidata.com>
2025-02-02 18:02:15 +0200

    Update started at

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -3,6 +3,6 @@
     {% set enabled = env_var('DBT_YUKI_ENABLED', 'True') %}
     {% set job = env_var('DBT_JOB_NAME') %}
     {% set started = run_started_at.strftime('%Y-%m-%dT%H:%M:%S') %}
-    {% set query_tag = '{"dbt_job":"' ~ job ~ '", "dbt_model":"' ~ model ~ '", "job_started_at":"' ~ job_started_at ~ '", "dbt_enabled":"' ~ enabled ~ '"}' %}
+    {% set query_tag = '{"dbt_job":"' ~ job ~ '", "dbt_model":"' ~ model ~ '", "job_started_at":"' ~ started ~ '", "dbt_enabled":"' ~ enabled ~ '"}' %}
 ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
 {% endmacro %}
