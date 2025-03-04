7438697165c9feb65f85038c1a411196266a81de
Alex Mirkin <alex@yukidata.com>
2025-03-04 15:31:35 +0200

    reset to original query tag after the query finishes use dispatch use full_refresh from is_incremental()

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,11 +1,41 @@
-{% macro set_query_tag() %}
+{% macro set_query_tag(extra = {}) -%}
+    {{ return(adapter.dispatch('set_query_tag', 'yuki_snowflake_dbt_tags')(extra=extra)) }}
+{%- endmacro %}
+
+{% macro default__set_query_tag(extra = {}) -%}
+    {% set original_query_tag = get_current_query_tag() %}
+
     {% set query_tag = {
         "dbt_job": env_var('DBT_JOB_NAME'),
         "dbt_model": model.name,
         "dbt_enabled": env_var('DBT_YUKI_ENABLED', true),
-        "full_refresh": flags.FULL_REFRESH,
         "invocation_id": invocation_id,
-        "run_cmd": flags.WHICH
-    } | tojson %}
-ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
+        "run_cmd": flags.WHICH,
+        "resource_type": model.resource_type,
+    } %}
+
+    {% if model.resource_type == 'model' %}
+        {%- do query_tag.update(
+            full_refresh=not is_incremental()
+        ) -%}
+    {% endif %}
+
+    {% set query_tag_json = tojson(query_tag) %}
+    {{ log("Setting query_tag to '" ~ query_tag_json ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
+    {% do run_query("alter session set query_tag = '{}'".format(query_tag_json)) %}
+    {{ return(original_query_tag)}}
+{% endmacro %}
+
+{% macro unset_query_tag(original_query_tag) -%}
+    {{ return(adapter.dispatch('unset_query_tag', 'yuki_snowflake_dbt_tags')(original_query_tag)) }}
+{%- endmacro %}
+
+{% macro default__unset_query_tag(original_query_tag) -%}
+    {% if original_query_tag %}
+    {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
+    {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
+    {% else %}
+    {{ log("No original query_tag, unsetting parameter.") }}
+    {% do run_query("alter session unset query_tag") %}
+    {% endif %}
 {% endmacro %}
