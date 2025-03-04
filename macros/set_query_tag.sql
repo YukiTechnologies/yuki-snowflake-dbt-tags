7e3a3b9f8904d652d5b5eb2dd96e5c3238610ad5
Alex Mirkin <alex@yukidata.com>
2025-03-04 15:39:41 +0200

    formatting

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -32,10 +32,10 @@
 
 {% macro default__unset_query_tag(original_query_tag) -%}
     {% if original_query_tag %}
-    {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
-    {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
+        {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
+        {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
     {% else %}
-    {{ log("No original query_tag, unsetting parameter.") }}
-    {% do run_query("alter session unset query_tag") %}
+        {{ log("No original query_tag, unsetting parameter.") }}
+        {% do run_query("alter session unset query_tag") %}
     {% endif %}
 {% endmacro %}
