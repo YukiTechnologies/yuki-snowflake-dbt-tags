ac09e331b04eaeab7a65cd941be603b4e663dabe
Alex Mirkin <alex@yukidata.com>
2025-03-04 13:19:34 +0200

    refactor to use tojson

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,17 +1,11 @@
 {% macro set_query_tag() %}
-{% set model = this.name %}
-    {% set enabled = env_var('DBT_YUKI_ENABLED', 'True') %}
-    {% set job = env_var('DBT_JOB_NAME') %}
-    {% set started = run_started_at.strftime('%Y-%m-%dT%H:%M:%S') %}
-    {% set query_tag = '{' %}
-    {% set query_tag = query_tag ~ '"dbt_job": "' ~ job ~ '",' %}
-    {% set query_tag = query_tag ~ '"dbt_model": "' ~ model ~ '",' %}
-    {% set query_tag = query_tag ~ '"job_started_at": "' ~ started ~ '",' %}
-    {% set query_tag = query_tag ~ '"dbt_enabled": ' ~ enabled | lower ~ ',' %}
-    {% set query_tag = query_tag ~ '"full_refresh": ' ~ flags.FULL_REFRESH | lower ~ ',' %}
-    {% set query_tag = query_tag ~ '"dbt_invocation_id": "' ~ invocation_id ~ '"' %}
-    {% set query_tag = query_tag ~ '}' %}
-    {% set query_tag_json = query_tag %}
-
-ALTER SESSION SET QUERY_TAG = '{{ query_tag_json }}';
+    {% set query_tag = {
+        "dbt_job": env_var('DBT_JOB_NAME'),
+        "dbt_model": model.name,
+        "dbt_enabled": env_var('DBT_YUKI_ENABLED', true),
+        "full_refresh": flags.FULL_REFRESH,
+        "invocation_id": invocation_id,
+        "run_cmd": flags.WHICH
+    } | tojson %}
+ALTER SESSION SET QUERY_TAG = '{{ query_tag }}';
 {% endmacro %}
